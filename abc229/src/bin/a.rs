6b8e25452fcb47529990e2bea9b77596a55b34d8
nabeen <watanabe_kenichiro@hasigo.co.jp>
2021-12-31 09:40:38 +0000

    abc229 a,b

--- abc229/src/bin/a.rs
+++ abc229/src/bin/a.rs
@@ -1,7 +1,28 @@
 #![allow(non_snake_case)]
 #![allow(unused_imports)]
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
-    todo!();
+    input! {
+        s1: Chars,
+        s2: Chars
+    }
+
+    let b: char = '#';
+
+    println!(
+        "{}",
+        if s1[0] == b && s1[1] == b {
+            "Yes"
+        } else if s2[0] == b && s2[1] == b {
+            "Yes"
+        } else if s1[0] == b && s2[0] == b {
+            "Yes"
+        } else if s1[1] == b && s2[1] == b {
+            "Yes"
+        } else {
+            "No"
+        }
+    )
 }
--- abc229/src/bin/b.rs
+++ abc229/src/bin/b.rs
@@ -1,7 +1,25 @@
 #![allow(non_snake_case)]
 #![allow(unused_imports)]
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
-    todo!();
+    input! {
+        a: isize,
+        b: isize,
+    }
+
+    let mut kuri = 0;
+    let mut aa = a;
+    let mut bb = b;
+    loop {
+        if aa <= 0 || bb <= 0 || kuri > 0 {
+            break;
+        }
+
+        kuri = (aa % 10 + bb % 10) / 10;
+        aa /= 10;
+        bb /= 10;
+    }
+    println!("{}", if kuri > 0 { "Hard" } else { "Easy" });
 }

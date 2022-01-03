667c3e9c5dc22f1c084635ce8e93c924cf6e3977
nabeen <watanabe_kenichiro@hasigo.co.jp>
2022-01-03 09:58:07 +0000

    abc227 a,b

--- abc227/src/bin/a.rs
+++ abc227/src/bin/a.rs
@@ -3,5 +3,19 @@
 use proconio::input;
 
 fn main() {
-    todo!();
+    input! {
+        n: i16,
+        k: i16,
+        a: i16,
+    }
+
+    let mut now = a;
+    for _i in 1..k {
+        if n == now {
+            now = 1;
+        } else {
+            now += 1;
+        }
+    }
+    println!("{}", now)
 }
--- abc227/src/bin/b.rs
+++ abc227/src/bin/b.rs
@@ -1,7 +1,30 @@
 #![allow(non_snake_case)]
 #![allow(unused_imports)]
 use proconio::input;
+use std::collections::HashSet;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        s: [usize; n],
+    }
+
+    // 4ab+3a+3b
+    let mut set: HashSet<usize> = HashSet::new();
+    for a in 1..150 {
+        for b in 1..150 {
+            if 4 * a * b + 3 * a + 3 * b <= 1000 {
+                set.insert(4 * a * b + 3 * a + 3 * b);
+            } else {
+                break;
+            }
+        }
+    }
+    let mut ans = 0;
+    for v in s {
+        if !set.contains(&v) {
+            ans += 1;
+        }
+    }
+    println!("{}", ans);
 }

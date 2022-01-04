0f5b2886ba8c88df2b58069b18a00253d0ce6e63
nabeen <watanabe_kenichiro@hasigo.co.jp>
2022-01-04 12:13:22 +0000

    abc225 a,b

--- abc225/src/bin/a.rs
+++ abc225/src/bin/a.rs
@@ -1,7 +1,21 @@
 #![allow(non_snake_case)]
 #![allow(unused_imports)]
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
-    todo!();
+    input! {
+        s: Chars
+    }
+
+    if s[0] == s[1] && s[1] == s[2] {
+        // 全て同じ
+        println!("{}", 1);
+    } else if s[0] == s[1] || s[1] == s[2] || s[2] == s[0] {
+        // 2つ同じ
+        println!("{}", 3);
+    } else {
+        // 全て異なる
+        println!("{}", 6);
+    }
 }
--- abc225/src/bin/b.rs
+++ abc225/src/bin/b.rs
@@ -1,7 +1,26 @@
 #![allow(non_snake_case)]
 #![allow(unused_imports)]
 use proconio::input;
+use proconio::marker::Chars;
+use std::collections::HashMap;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [[usize; 2]; n-1],
+    }
+
+    let mut map = HashMap::new();
+    for v in a {
+        *map.entry(v[0]).or_insert(0) += 1;
+        *map.entry(v[1]).or_insert(0) += 1;
+    }
+    println!(
+        "{}",
+        if *map.iter().max_by(|x, y| x.1.cmp(y.1)).unwrap().1 == n - 1 {
+            "Yes"
+        } else {
+            "No"
+        }
+    );
 }

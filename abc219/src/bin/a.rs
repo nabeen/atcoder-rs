0b666bb835cee50cb9f302c0d3b12f539b22dd3d
nabeen <nabeen@users.noreply.github.com>
2022-01-10 01:52:24 +0900

    abc219 a,b

--- abc219/src/bin/a.rs
+++ abc219/src/bin/a.rs
@@ -3,5 +3,14 @@
 use proconio::input;
 
 fn main() {
-    todo!();
+    input! {
+        x: usize,
+    }
+
+    match x {
+        0..=39 => println!("{}", 40 - x),
+        40..=69 => println!("{}", 70 - x),
+        70..=89 => println!("{}", 90 - x),
+        _ => println!("expert"),
+    }
 }
--- abc219/src/bin/b.rs
+++ abc219/src/bin/b.rs
@@ -1,7 +1,25 @@
 #![allow(non_snake_case)]
 #![allow(unused_imports)]
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
-    todo!();
+    input! {
+        s1: String,
+        s2: String,
+        s3: String,
+        t: Chars
+    }
+
+    let mut s = String::new();
+    for i in t.iter() {
+        match i {
+            '1' => s += &s1,
+            '2' => s += &s2,
+            '3' => s += &s3,
+            _ => (),
+        }
+    }
+
+    println!("{}", s)
 }

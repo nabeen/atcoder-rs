dced4c38db6c466dc1e8e391dba75b6baa560422
nabeen <nabeen@users.noreply.github.com>
2022-01-10 01:25:33 +0900

    abc221 b and mod toml

--- abc221/src/bin/b.rs
+++ abc221/src/bin/b.rs
@@ -1,7 +1,26 @@
 #![allow(non_snake_case)]
 #![allow(unused_imports)]
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
-    todo!();
+    input! {
+        mut s: Chars,
+        t: Chars
+    }
+
+    if s == t {
+        println!("Yes");
+        return;
+    }
+
+    for i in 0..(s.len() - 1) {
+        s.swap(i, i + 1);
+        if s == t {
+            println!("Yes");
+            return;
+        }
+        s.swap(i, i + 1);
+    }
+    println!("No");
 }

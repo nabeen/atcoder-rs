574e27afc57b0acc70021371ea192b07925a49d6
nabeen <watanabe.kenichiro@bell-face.com>
2022-01-11 11:36:22 +0900

    abc218 a,b

--- abc218/src/bin/a.rs
+++ abc218/src/bin/a.rs
@@ -1,7 +1,14 @@
 #![allow(non_snake_case)]
 #![allow(unused_imports)]
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        s: String
+    }
+
+    let s = &s[n - 1..n];
+    println!("{}", if s == "o" { "Yes" } else { "No" })
 }
--- abc218/src/bin/b.rs
+++ abc218/src/bin/b.rs
@@ -1,7 +1,20 @@
 #![allow(non_snake_case)]
 #![allow(unused_imports)]
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
-    todo!();
+    input! {
+        p: [usize; 26]
+    }
+
+    let alphabet = vec![
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+        's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    ];
+    let mut ans = String::new();
+    for v in p {
+        ans += &alphabet[v - 1].to_string();
+    }
+    println!("{}", ans);
 }

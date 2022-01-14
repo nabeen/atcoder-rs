2807116b26d2c04eaf77697b1ddb94e9e0d55eb6
nabeen <watanabe.kenichiro@bell-face.com>
2022-01-14 10:21:53 +0900

    abc 216 a,b

--- abc216/src/bin/a.rs
+++ abc216/src/bin/a.rs
@@ -1,7 +1,25 @@
 #![allow(non_snake_case)]
 #![allow(unused_imports)]
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
-    todo!();
+    input! {
+        xy: String
+    }
+
+    let xy: Vec<_> = xy.split('.').collect();
+    let x: usize = xy[0].parse().unwrap();
+    let y: usize = xy[1].parse().unwrap();
+
+    println!(
+        "{}{}",
+        x,
+        match y {
+            0..=2 => "-",
+            3..=6 => "",
+            7..=9 => "+",
+            _ => "",
+        }
+    )
 }
--- abc216/src/bin/b.rs
+++ abc216/src/bin/b.rs
@@ -1,7 +1,27 @@
 #![allow(non_snake_case)]
 #![allow(unused_imports)]
 use proconio::input;
+use std::collections::HashSet;
+
+#[derive(Debug, PartialEq, Eq, Hash)]
+struct Person {
+    m: String,
+    n: String,
+}
 
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        st: [(String, String);n]
+    }
+
+    let map: HashSet<Person> = st
+        .iter()
+        .map(|p| Person {
+            n: p.0.clone(),
+            m: p.1.clone(),
+        })
+        .collect();
+
+    println!("{}", if map.len() == st.len() { "No" } else { "Yes" });
 }

6c0ced3ed9555df706ba8c4ff951c3a066382f18
nabeen <nabeen@users.noreply.github.com>
2022-01-15 22:34:04 +0900

    abc234 a,b

--- abc234/src/bin/b.rs
+++ abc234/src/bin/b.rs
@@ -1,7 +1,22 @@
 #![allow(non_snake_case)]
 #![allow(unused_imports)]
+use num_integer::Roots;
 use proconio::input;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        xy: [[f64;2];n]
+    }
+
+    let mut max = 0_f64;
+    for i in 0..xy.len() - 1 {
+        for j in i..xy.len() {
+            let a = &xy[i];
+            let b = &xy[j];
+            let tmp = ((a[0] - b[0]).abs().powi(2) + (a[1] - b[1]).abs().powi(2)).sqrt();
+            max = if tmp > max { tmp } else { max }
+        }
+    }
+    println!("{}", max);
 }

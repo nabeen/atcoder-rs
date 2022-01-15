280848947cab3e2791055ced9817ce2199f1d0df
nabeen <nabeen@users.noreply.github.com>
2022-01-15 16:22:35 +0900

    abc234 a

--- abc234/src/bin/a.rs
+++ abc234/src/bin/a.rs
@@ -2,6 +2,14 @@
 #![allow(unused_imports)]
 use proconio::input;
 
+fn f(t: usize) -> usize {
+    t * t + 2 * t + 3
+}
+
 fn main() {
-    todo!();
+    input! {
+        t: usize
+    }
+
+    println!("{}", f(f(f(t) + t) + f(f(t))))
 }

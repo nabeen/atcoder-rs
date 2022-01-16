ee2f0fc8bdaffba73a429873d425a63dc7e4dd69
nabeen <nabeen@users.noreply.github.com>
2022-01-16 15:51:59 +0900

    abc217 c

--- abc217/src/bin/c.rs
+++ abc217/src/bin/c.rs
@@ -1,7 +1,22 @@
 #![allow(non_snake_case)]
 #![allow(unused_imports)]
 use proconio::input;
+use std::collections::HashMap;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        p: [usize; n]
+    }
+
+    let mut p = p.iter().enumerate().collect::<Vec<_>>();
+    p.sort_by_key(|&(_i, v)| v);
+    p.iter().for_each(|&(i, x)| {
+        if *x == 1 {
+            print!("{}", i + 1)
+        } else {
+            print!(" {}", i + 1)
+        }
+    });
+    println!("");
 }

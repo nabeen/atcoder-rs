f4807f6d71ed662562914aa9c785b004a3cda70b
nabeen <nabeen@users.noreply.github.com>
2022-01-17 22:01:23 +0900

    abc216 c

--- abc216/src/bin/c.rs
+++ abc216/src/bin/c.rs
@@ -1,7 +1,24 @@
 #![allow(non_snake_case)]
 #![allow(unused_imports)]
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
-    todo!();
+    input! {
+        mut n: usize
+    }
+
+    // let mut now = n;
+    let mut ans = vec![];
+    while n != 0 {
+        if n % 2 == 0 {
+            n /= 2;
+            ans.push('B');
+        } else {
+            n -= 1;
+            ans.push('A');
+        }
+    }
+
+    println!("{}", ans.into_iter().rev().join(""));
 }

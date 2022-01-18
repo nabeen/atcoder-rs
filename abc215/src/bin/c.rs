0ce8a5630cfa69b12f52f2792d547bd403fb9c79
nabeen <watanabe.kenichiro@bell-face.com>
2022-01-18 10:26:32 +0900

    acb215 c

--- abc215/src/bin/c.rs
+++ abc215/src/bin/c.rs
@@ -1,7 +1,22 @@
 #![allow(non_snake_case)]
 #![allow(unused_imports)]
+use itertools::Itertools;
 use proconio::input;
+use proconio::marker::Chars;
+use std::collections::HashSet;
 
 fn main() {
-    todo!();
+    input! {
+        s: String,
+        k: usize,
+    }
+
+    let mut hash = HashSet::new();
+    for perm in s.chars().permutations(s.len()) {
+        hash.insert(perm.iter().join(""));
+    }
+    let mut dic = hash.into_iter().collect::<Vec<String>>();
+    dic.sort();
+
+    println!("{}", dic[k - 1]);
 }

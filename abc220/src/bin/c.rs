ce99795256de6f37928e6f83af139aeacfaf0ab7
nabeen <nabeen@users.noreply.github.com>
2022-01-18 23:16:14 +0900

    abc220 c

--- abc220/src/bin/c.rs
+++ abc220/src/bin/c.rs
@@ -3,5 +3,25 @@
 use proconio::input;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [usize; n],
+        x: usize,
+    }
+
+    // 一旦aのシグマを計算しておく
+    let sum: usize = a.iter().sum();
+
+    let p = x / sum;
+    let mut sumb = p * sum;
+    let mut ans = p * n;
+
+    for v in a {
+        sumb += v;
+        ans += 1;
+        if sumb > x {
+            break;
+        }
+    }
+    println!("{}", ans);
 }

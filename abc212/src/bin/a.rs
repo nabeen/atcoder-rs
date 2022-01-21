5e4bc2f59aba93751da66ecfb6ed331a1a9f6b27
nabeen <watanabe.kenichiro@bell-face.com>
2022-01-21 18:42:09 +0900

    abc212 a,b

--- abc212/src/bin/a.rs
+++ abc212/src/bin/a.rs
@@ -3,5 +3,21 @@
 use proconio::input;
 
 fn main() {
-    todo!();
+    input! {
+        a: usize,
+        b: usize,
+    }
+
+    println!(
+        "{}",
+        if 0 < a && b == 0 {
+            "Gold"
+        } else if a == 0 && 0 < b {
+            "Silver"
+        } else if 0 < a && 0 < b {
+            "Alloy"
+        } else {
+            ""
+        }
+    )
 }
--- abc212/src/bin/b.rs
+++ abc212/src/bin/b.rs
@@ -1,7 +1,34 @@
 #![allow(non_snake_case)]
 #![allow(unused_imports)]
+use std::collections::HashSet;
+use std::iter::FromIterator;
+
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
-    todo!();
+    input! {
+        x: Chars,
+    }
+
+    let mut ans = "Strong";
+    // 1st
+    let uniq: HashSet<&char> = HashSet::from_iter(x.iter());
+    if uniq.len() == 1 {
+        ans = "Weak";
+    }
+    let mut plus = true;
+    for i in 0..x.len() - 1 {
+        if (x[i].to_string().parse::<usize>().unwrap() + 1) % 10
+            == x[i + 1].to_string().parse::<usize>().unwrap()
+        {
+        } else {
+            plus = false;
+        }
+    }
+    if plus {
+        ans = "Weak";
+    }
+
+    println!("{}", ans);
 }

bab0b30f37bdf4419318ab1fb7cf266e92e8c210
nabeen <watanabe.kenichiro@bell-face.com>
2022-01-24 18:21:57 +0900

    abc236 a,b

--- abc236/src/bin/a.rs
+++ abc236/src/bin/a.rs
@@ -1,7 +1,16 @@
 #![allow(non_snake_case)]
 #![allow(unused_imports)]
+use itertools::Itertools;
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
-    todo!();
+    input! {
+        mut s: Chars,
+        a: usize,
+        b: usize,
+    }
+
+    s.swap(a - 1, b - 1);
+    println!("{}", s.iter().join(""));
 }
--- abc236/src/bin/b.rs
+++ abc236/src/bin/b.rs
@@ -1,7 +1,17 @@
 #![allow(non_snake_case)]
 #![allow(unused_imports)]
 use proconio::input;
+use std::collections::HashMap;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        mut a: [usize; 4*n-1]
+    }
+
+    let mut map = HashMap::new();
+    for i in a {
+        *map.entry(i).or_insert(0) += 1;
+    }
+    println!("{}", map.iter().min_by(|x, y| x.1.cmp(y.1)).unwrap().0)
 }

39a1127adaaaab83446ee053e980e9669e64b0b4
nabeen <watanabe.kenichiro@bell-face.com>
2022-01-25 12:31:44 +0900

    abc236 c

--- abc236/src/bin/c.rs
+++ abc236/src/bin/c.rs
@@ -1,7 +1,21 @@
 #![allow(non_snake_case)]
 #![allow(unused_imports)]
 use proconio::input;
+use std::collections::HashSet;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        m: usize,
+        s: [String; n],
+        t: [String; m],
+    }
+
+    let mut set = HashSet::new();
+    for v in t {
+        set.insert(v);
+    }
+    for v in s {
+        println!("{}", if set.contains(&v) { "Yes" } else { "No" })
+    }
 }

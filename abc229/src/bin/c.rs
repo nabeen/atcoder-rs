36d0eb84850c82a878d9a7c221631a96233fa92f
nabeen <watanabe.kenichiro@bell-face.com>
2022-01-25 17:29:25 +0900

    abc229 c

--- abc229/src/bin/c.rs
+++ abc229/src/bin/c.rs
@@ -3,5 +3,30 @@
 use proconio::input;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        w: usize,
+        mut ab: [(usize, usize); n],
+    }
+
+    ab.sort_by(|x, y| y.0.cmp(&x.0));
+
+    let mut ans = 0;
+    let mut gram = 0;
+    for (a, b) in ab {
+        if gram + b <= w {
+            ans += a * b;
+            gram += b;
+        } else {
+            loop {
+                if gram + 1 > w {
+                    break;
+                } else {
+                    ans += a;
+                    gram += 1;
+                }
+            }
+        }
+    }
+    println!("{}", ans);
 }

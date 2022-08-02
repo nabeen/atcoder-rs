07280caf6acf310e85bc042b67aba7122abb452d
nabeen <nabeen@users.noreply.github.com>
2022-08-03 02:31:44 +0900

    261 a,b

--- abc261/src/bin/a.rs
+++ abc261/src/bin/a.rs
@@ -1,7 +1,16 @@
 #![allow(non_snake_case)]
 #![allow(unused_imports)]
+use std::cmp::{max, min};
+
 use proconio::input;
 
 fn main() {
-    todo!();
+    input! {
+        l1: isize,
+        r1: isize,
+        l2: isize,
+        r2: isize,
+    }
+
+    println!("{}", max(0, min(r1, r2) - max(l1, l2)))
 }
--- abc261/src/bin/b.rs
+++ abc261/src/bin/b.rs
@@ -1,7 +1,34 @@
 #![allow(non_snake_case)]
 #![allow(unused_imports)]
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [Chars;n]
+    }
+
+    for i in 0..n {
+        for j in i+1..n {
+            if a[i][j] == 'W' {
+                if a[j][i] != 'L' {
+                    println!("incorrect");
+                    return    
+                }
+            }
+            if a[i][j] == 'L' {
+                if a[j][i] != 'W' {
+                    println!("incorrect");
+                    return    
+                }
+            }
+            if a[i][j] == 'D' {
+                if a[j][i] != 'D' {
+                    println!("incorrect");
+                    return    
+                }
+            }
+        }
+    }
+    println!("correct");
 }

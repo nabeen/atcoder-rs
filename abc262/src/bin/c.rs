32c6b592ba16f35e3c890d0fd98c492c1f1855ab
nabeen <nabeen@users.noreply.github.com>
2022-08-04 00:10:21 +0900

    tle

--- abc262/src/bin/c.rs
+++ abc262/src/bin/c.rs
@@ -1,7 +1,25 @@
 #![allow(non_snake_case)]
 #![allow(unused_imports)]
+use std::cmp::{min, max};
+
 use proconio::input;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [usize;n]
+    }
+
+    let mut ans = 0;
+    for i in 0..n {
+        for j in 0..n {
+            if i == j {
+                continue;
+            }
+            if min(a[i], a[j]) == i+1 && max(a[i], a[j]) == j+1 && (1 <= i+1 && i+1 <= j+1 && j+1 <= n) {
+                ans+=1;
+            }
+        }
+    }
+    println!("{}", ans);
 }

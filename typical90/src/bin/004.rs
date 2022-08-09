1bc8388f4dc3ae56c42691dd0389842675798a11
nabeen <nabeen@users.noreply.github.com>
2022-08-09 23:34:12 +0900

    4,10

--- typical90/src/bin/004.rs
+++ typical90/src/bin/004.rs
@@ -3,5 +3,27 @@
 use proconio::input;
 
 fn main() {
-    todo!();
+    input! {
+        h: usize,
+        w: usize,
+        a: [[usize;w];h]
+    }
+
+    let mut row = vec![0; h];
+    let mut col = vec![0; w];
+
+    for i in 0..h {
+        for j in 0..w {
+            row[i] += a[i][j];
+            col[j] += a[i][j];
+        }
+    }
+
+    for i in 0..h {
+        let mut tmp: Vec<String> = vec![];
+        for j in 0..w {
+            tmp.push((row[i] + col[j] - a[i][j]).to_string())
+        }
+        println!("{}", tmp.join(" "));
+    }
 }
--- typical90/src/bin/010.rs
+++ typical90/src/bin/010.rs
@@ -1,7 +1,33 @@
 #![allow(non_snake_case)]
 #![allow(unused_imports)]
+use proconio::fastout;
 use proconio::input;
 
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        cp: [(usize, usize);n],
+        q: usize,
+        lr: [(usize, usize);q],
+    }
+
+    // 累積和保持用
+    let mut s1 = vec![0; n + 1];
+    let mut s2 = vec![0; n + 1];
+
+    // 累積和
+    for (i, &(c, p)) in cp.iter().enumerate() {
+        if c == 1 {
+            s1[i + 1] = s1[i] + p;
+            s2[i + 1] = s2[i];
+        } else if c == 2 {
+            s1[i + 1] = s1[i];
+            s2[i + 1] = s2[i] + p;
+        }
+    }
+
+    for (l, r) in lr {
+        println!("{} {}", s1[r] - s1[l - 1], s2[r] - s2[l - 1]);
+    }
 }

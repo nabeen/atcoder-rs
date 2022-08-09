90d8b708caccd3b8f701755b81fe3539e6de6482
nabeen <nabeen@users.noreply.github.com>
2022-08-10 00:17:04 +0900

    gcd

--- typical90/src/bin/022.rs
+++ typical90/src/bin/022.rs
@@ -1,7 +1,17 @@
 #![allow(non_snake_case)]
 #![allow(unused_imports)]
+use num::integer::gcd;
+use proconio::fastout;
 use proconio::input;
 
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        a: usize,
+        b: usize,
+        c: usize,
+    }
+
+    let r = gcd(a, gcd(b, c));
+    println!("{}", (a / r - 1) + (b / r - 1) + (c / r - 1))
 }

abd0103b45d05612c1f08269662e49d59b3de04b
nabeen <nabeen@users.noreply.github.com>
2022-08-11 00:49:50 +0900

    024,027

--- typical90/src/bin/024.rs
+++ typical90/src/bin/024.rs
@@ -1,7 +1,28 @@
 #![allow(non_snake_case)]
 #![allow(unused_imports)]
+
+use num::abs;
 use proconio::input;
 
 fn main() {
-    todo!();
+    input! {
+        n: i64,
+        k: i64,
+        a: [i64; n],
+        b: [i64; n],
+    }
+
+    let diff = a
+        .iter()
+        .zip(b.iter())
+        .fold(0, |acc, x| acc + (x.0 - x.1).abs());
+
+    println!(
+        "{}",
+        if k >= diff && (k - diff) % 2 == 0 {
+            "Yes"
+        } else {
+            "No"
+        }
+    )
 }
--- typical90/src/bin/027.rs
+++ typical90/src/bin/027.rs
@@ -1,7 +1,24 @@
 #![allow(non_snake_case)]
 #![allow(unused_imports)]
-use proconio::input;
+use proconio::marker::Chars;
+use proconio::{fastout, input};
+use std::collections::HashMap;
 
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        s: [String; n]
+    }
+
+    // println!("{:?}", s)
+    let mut map = HashMap::new();
+    for (i, k) in s.iter().enumerate() {
+        if map.contains_key(k) {
+            continue;
+        } else {
+            map.insert(k, true);
+            println!("{}", i + 1);
+        }
+    }
 }

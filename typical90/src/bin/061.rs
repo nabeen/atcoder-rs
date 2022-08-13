f93e2be34357cb9dc15dc723052c727ee18808db
nabeen <nabeen@users.noreply.github.com>
2022-08-13 23:59:46 +0900

    061

--- typical90/src/bin/061.rs
+++ typical90/src/bin/061.rs
@@ -1,7 +1,25 @@
 #![allow(non_snake_case)]
 #![allow(unused_imports)]
-use proconio::input;
+use proconio::{fastout, input};
+use std::collections::VecDeque;
 
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        q: usize,
+        tx: [(usize, usize); q]
+    }
+
+    let mut que = VecDeque::new();
+    for (t, x) in tx {
+        if t == 1 {
+            que.push_front(x);
+        }
+        if t == 2 {
+            que.push_back(x);
+        }
+        if t == 3 {
+            println!("{}", que[x - 1]);
+        }
+    }
 }

7207a180b68961e91762d03e4abf5758a05b4afc
nabeen <nabeen@users.noreply.github.com>
2022-08-13 23:23:06 +0900

    055

--- typical90/src/bin/055.rs
+++ typical90/src/bin/055.rs
@@ -3,5 +3,27 @@
 use proconio::input;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        p: usize,
+        q: usize,
+        a: [usize;n],
+    }
+
+    // println!("{:?}", a)
+    let mut ans = 0;
+    for i in 0..n - 4 {
+        for j in i + 1..n - 3 {
+            for k in j + 1..n - 2 {
+                for l in k + 1..n - 1 {
+                    for m in l + 1..n {
+                        if (a[i] % p * a[j] % p * a[k] % p * a[l] % p * a[m] % p) == q {
+                            ans += 1
+                        }
+                    }
+                }
+            }
+        }
+    }
+    println!("{}", ans);
 }
